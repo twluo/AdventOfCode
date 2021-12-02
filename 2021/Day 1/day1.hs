9514af3a134fca6854faabc153355c8e25107f1f
Tony Luo <tonywluo@gmail.com>
2021-12-02 01:25:35 -0800

    Cleaning up a lil bit

--- 2021/Day 1/day1.hs
+++ 2021/Day 1/day1.hs
@@ -8,14 +8,19 @@ countIncreasingDepths x = increasingCount where
     justNeg = filter (<0) depthDiff
     increasingCount = length justNeg
 
+-- Part 1
+solve1 :: [Int] -> Int
+solve1 input = countIncreasingDepths input
+
+-- Part 2
+solve2:: [Int] -> Int
+solve2 input = sol where
+    depthTriplets = zip3 input (tail input) (tail (tail input))
+    depthTripletsSum = map (\(x, y, z) -> x + y + z) depthTriplets
+    sol = countIncreasingDepths depthTripletsSum
+
 main = do
     input <- readFile "input.txt"
     let depths = map (read::String->Int) (splitOn "\r\n" input)
-
-    -- Part 1
-    print (countIncreasingDepths depths)
-
-    -- Part 2
-    let depthTriplets = zip3 depths (tail depths) (tail (tail depths))
-        depthTripletsSum = map (\(x, y, z) -> x + y + z) depthTriplets
-    print (countIncreasingDepths depthTripletsSum)
+    print $ solve1 depths
+    print $ solve2 depths
--- 2021/Day 2/day2.hs
+++ 2021/Day 2/day2.hs
@@ -4,12 +4,6 @@ processInput :: String -> (String, Int)
 processInput line = (direction, ((read::String->Int) value)) where
     direction:value:rest = splitOn " " line
 
-solve1 :: [(String, Int)] -> Int
-solve1 lines = direction * value where (direction, value) = (findDepthAndDist lines (0, 0))
-
-solve2 :: [(String, Int)] -> Int
-solve2 lines = direction * value where (direction, value, _) = (findDepthAndDistAndAim lines (0, 0, 0))
-
 findDepthAndDist :: [(String, Int)] -> (Int, Int) -> (Int, Int)
 findDepthAndDist [] acc = acc
 findDepthAndDist ((direction, value):rest) (depth, dist)
@@ -23,6 +17,14 @@ findDepthAndDistAndAim ((direction, value):rest) (depth, dist, aim)
     | direction == "forward" = findDepthAndDistAndAim rest (depth + aim * value, dist + value, aim)
     | direction == "down" = findDepthAndDistAndAim rest (depth, dist, aim + value)
     | direction == "up" = findDepthAndDistAndAim rest (depth, dist, aim - value)
+
+-- Part 1
+solve1 :: [(String, Int)] -> Int
+solve1 lines = direction * value where (direction, value) = (findDepthAndDist lines (0, 0))
+
+-- Part 2
+solve2 :: [(String, Int)] -> Int
+solve2 lines = direction * value where (direction, value, _) = (findDepthAndDistAndAim lines (0, 0, 0))
 
 main = do
     input <- readFile "input.txt"
